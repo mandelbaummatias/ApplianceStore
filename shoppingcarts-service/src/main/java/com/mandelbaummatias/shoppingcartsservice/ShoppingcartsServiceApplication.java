b15a04c2dc99760ee301800f0680df85c6c839a6
Matias Mandelbaum <105255748+mandelbaummatias@users.noreply.github.com>
2023-12-16 11:50:31 -0300

    Added application.properties

---
+++ shoppingcarts-service/src/main/java/com/mandelbaummatias/shoppingcartsservice/ShoppingcartsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mandelbaummatias.shoppingcartsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartsServiceApplication.class, args);
+	}
+
+}
---
+++ shoppingcarts-service/src/test/java/com/mandelbaummatias/shoppingcartsservice/ShoppingcartsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandelbaummatias.shoppingcartsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
