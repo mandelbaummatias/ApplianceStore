713872080a8327631c34a1eb6c4125523c76dfdf
Matias <mandelbaum.matias@gmail.com>
2023-12-18 14:20:08 -0300

    Fix api-gateway application.properties

--- api-gateway/src/main/java/com/mandelbaummatias/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/mandelbaummatias/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,10 @@ package com.mandelbaummatias.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class ApiGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
